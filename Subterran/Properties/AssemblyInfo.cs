68423b0dc206e42c57059cf7f8e1763b6595e558
Layl Conway <laylconway@live.com>
2015-01-07 17:16:06 +0100

    Updated AssemblyInfo in Subterran.

--- Subterran/Properties/AssemblyInfo.cs
+++ Subterran/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Subterran")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An open source 3D game engine library written in C#.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("EdgeWorks Games")]
 [assembly: AssemblyProduct("Subterran")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © EdgeWorks Games 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
