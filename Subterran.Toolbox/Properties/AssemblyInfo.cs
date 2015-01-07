bc2b193ae7977667f39a7f8aaff3dfa8ccd2cf9d
Layl Conway <laylconway@live.com>
2015-01-07 16:34:25 +0100

    Updated assembly data for Subterran.Toolbox.

--- Subterran.Toolbox/Properties/AssemblyInfo.cs
+++ Subterran.Toolbox/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Subterran.Basic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Subterran.Toolbox")]
+[assembly: AssemblyDescription("A set of tools for building a game based on the Subterran game engine.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Subterran.Basic")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("EdgeWorks Games")]
+[assembly: AssemblyProduct("Subterran.Toolbox")]
+[assembly: AssemblyCopyright("Copyright © EdgeWorks Games 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
