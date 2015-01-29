ed67ab7c9310c0ec07d35d03ba7187c40ba63763
Layl Conway <laylconway@live.com>
2015-01-29 11:45:34 +0100

    Added example entity for testing.

---
+++ Subterran.Toolbox/BasicEntities.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using Subterran.Rendering.Components;
+using Subterran.Toolbox.Voxels;
+
+namespace Subterran.Toolbox
+{
+	public static class BasicEntities
+	{
+		public static Entity CreateTestBlockEntity()
+		{
+			return new Entity
+			{
+				Components =
+				{
+					new MeshRendererComponent(),
+					new VoxelMapComponent<ColoredVoxel>
+					{
+						MeshGenerator = ColoredVoxelMesher.MeshGenerator,
+						Voxels = new[,,]
+						{
+							{
+								{
+									new ColoredVoxel
+									{
+										IsSolid = true,
+										Color = StMath.NormalizeColor(Color.DarkRed)
+									}
+								}
+							}
+						}
+					}
+				}
+			};
+		}
+	}
+}
