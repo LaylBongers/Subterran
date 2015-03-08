58656ff6b6bdd36dc365dc6d7e14d9a0b343f4d2
Layl Conway <laylconway@live.com>
2015-03-08 17:11:17 +0100

    This should hopefully finally make coveralls work.

---
+++ ScanCoverage.bat
@@ -0,0 +1,1 @@
+.\packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user -target:"packages\xunit.runners.2.0.0-rc3-build2880\tools\xunit.console.exe" -targetargs:"Subterran.Tests\bin\Debug\Subterran.Tests.dll -noshadow -appveyor" -filter:"+[Subterran*]*" -output:coverage.xml
